e40005ada0067bb284625bdb5deddf72302600c9
harsh <harsh710pat@gmail.com>
2020-10-08 18:11:28 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import "./App.css";
-import Users from "./components/users/Users";
-import axios from "axios";
+import User from "./components/users/User";
+import Alert from "./components/layout/Alert";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
+import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
 
-class App extends Component {
-  state = {
-    users: [],
-    loading: false,
-  };
-  async componentDidMount() {
-    this.setState({ loading: true });
-    const res = await axios.get("http://api.github.com/users");
-    console.log(res.data);
-    this.setState({ users: res.data, loading: false });
-  }
-  render() {
-    return (
-      <div className="App">
-        <Navbar title="Github Finder" icon="fab fa-github" />
-        <div className="container">
-          <Users loading={this.state.loading} users={this.state.users} />
-        </div>
-      </div>
-    );
-  }
-}
+import "./App.css";
+
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+};
 
 export default App;
+
+// import React, { useState, Fragment, useEffect } from "react";
+// import Navbar from "./components/layout/Navbar";
+// import "./App.css";
+// import User from "./components/users/User";
+// import Users from "./components/users/Users";
+// import Search from "./components/users/Search";
+// import Alert from "./components/layout/Alert";
+// import About from "./components/pages/About";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import GithubState from "./context/github/GithubState";
+// import AlertState from "./context/alert/AlertState";
+// import Home from "./components/pages/Home";
+
+// const App = () => {
+//   return (
+//     <GithubState>
+//       <AlertState>
+//         <Router>
+//           <div className="App">
+//             <Navbar title="Github Finder" icon="fab fa-github" />
+//             <div className="container">
+//               <Alert />
+//               <Switch>
+//                 <Route path="/" component={Home} />
+//               </Switch>
+
+//               <Route exact path="/about" component={About} />
+//               <Route exact path="/user/:login" component={User} />
+//             </div>
+//           </div>
+//         </Router>
+//       </AlertState>
+//     </GithubState>
+//   );
+// };
+
+// export default App;
